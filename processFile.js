fb330dd3148ce69f2338241bc8b1d615787d3e3a
Fernando Chucre <fernandochucre@gmail.com>
2016-03-08 22:36:46 -0300

    fix gitignore and file path

--- processFile.js
+++ processFile.js
@@ -2,7 +2,7 @@ var trackingParser = require('./tracking-parser'),
     mapMatching = require('./map-matching'),
     timeTravelCalculate = require('./time-travel-calculate');
 
-trackingParser("./sample/points.txt", function(traking){
+trackingParser("./samples/points.txt", function(traking){
   mapMatching(traking, function(route, traking){
     timeTravelCalculate(route, traking, function(route){
       route.links.map(function(e){
