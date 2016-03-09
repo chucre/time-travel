d509e18c238028ad4bdc15d9346b3669daf195e7
Fernando Chucre <fernandochucre@gmail.com>
2016-03-08 22:32:33 -0300

    Update initial instructions and offer a sample file

--- processFile.js
+++ processFile.js
@@ -2,7 +2,7 @@ var trackingParser = require('./tracking-parser'),
     mapMatching = require('./map-matching'),
     timeTravelCalculate = require('./time-travel-calculate');
 
-trackingParser("/Users/fernandochucre/tmp/points_2015-11-19.10-01-01.traking.txt", function(traking){
+trackingParser("./sample/points.txt", function(traking){
   mapMatching(traking, function(route, traking){
     timeTravelCalculate(route, traking, function(route){
       route.links.map(function(e){
--- tracking-parser.js
+++ tracking-parser.js
@@ -11,20 +11,19 @@ module.exports = function(filename, cb) {
     console.log(err.toString());
   })
   buffer.on('data', function(_data){
-    if (returned) return;
     data += _data;
     var indexOf = data.indexOf("\n");
     while (indexOf>=0) {
       var line = data.substring(0,indexOf).split(';');
       var track = [line[0],line[1],line[2],new Date(line[3]).getTime()];
       data = data.substring(indexOf+1, data.length);
-      
+
       if (last_track && last_track[0]!=track[0]) {
         tracking.sort(function(a,b){
           return a[3]-b[3];
         })
         for(var i=1; i<tracking.length; i++) {
-          if (tracking[i-1][1]==tracking[i][1] 
+          if (tracking[i-1][1]==tracking[i][1]
             && tracking[i-1][2]==tracking[i][2]
             && tracking[i-1][3]==tracking[i][3]) {
             tracking.splice(i, 1);
@@ -32,8 +31,6 @@ module.exports = function(filename, cb) {
           }
         }
         cb.call(this, tracking);
-        returned=true;
-        return;
         tracking = [];
         last_track=undefined;
       }
