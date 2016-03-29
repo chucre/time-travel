655478f1aa60eb7855861d65f20180e604526a08
Mirla Braga <mirlabraga@gmail.com>
2016-03-29 00:18:31 -0300

    fix i indenting

--- processFile.js
+++ processFile.js
@@ -11,15 +11,15 @@ trackingParser(file_name, function(traking){
   mapMatching(traking, function(route, traking){
     timeTravelCalculate(route, traking, function(route){
       route.links.map(function(e){
-	var geometry = JSON.parse(e.geometry);
-	pg.connect(conString, function(err, client, done) {
-	  client.query('select count(*) as total from edgelatlng where idedge = $1::int', [e.id], function(err, result) {
-	    if (err) {
+         var geometry = JSON.parse(e.geometry);
+         pg.connect(conString, function(err, client, done) {
+           client.query('select count(*) as total from edgelatlng where idedge = $1::int', [e.id], function(err, result) {
+             if (err) {
               console.log(err);
             }
-	    if (result.rows[0].total==0) {
+            if (result.rows[0].total==0) {
               var j = geometry.coordinates.length-1;
-              client.query('insert into edgelatlng (idedge, geometry) values ($1::int, $2);', [e.id, e.geometry], function(err, result){
+              client.query('insert into edgelatlng (idedge, geometry) values ($1::int, $2);', [e.id, geometry.coordinates], function(err, result) {
                 if (err) {
                   console.log(err);
                 }
@@ -28,16 +28,16 @@ trackingParser(file_name, function(traking){
             } else {
               done();
             }
-	  });
+          });
         });
         pg.connect(conString, function(err, client, done) {
-	  client.query('insert into edgeid (idedge, timeday, totaltime) values ($1, $2, $3)',[e.id, e.entry_time, e.total_time], function(err, result){
-           if (err) {
+          client.query('insert into edgeid (idedge, timeday, totaltime) values ($1, $2, $3)',[e.id, e.entry_time, e.total_time], function(err, result){
+            if (err) {
               console.log(err);
-           }
-           done();
+            }
+            done();
           });
-	});
+        });
         console.log(e.id+';'+JSON.stringify(geometry.coordinates)+';'+e.total_time+';'+e.totalLength+';'+e.entry_time);
       })
     });
